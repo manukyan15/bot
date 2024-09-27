d4e4f79aa23aff12b1148ab53152a1b2605a9202
manukyan15 <100085386+manukyan15@users.noreply.github.com>
2024-09-27 21:44:33 +0400

    Update main.js

--- main.js
+++ main.js
@@ -224,6 +224,32 @@ botThre.on('callback_query', (callbackQuery) => {
     }
 });
 
+const TelegramBot = require('node-telegram-bot-api'); 
+
+const tokenFore = '7935393167:AAGm2MgowSZSo-zH2x3I8CsKmwXkSFPG0ds';
+
+const botFore = new TelegramBot(tokenFore, {polling: true});
+
+botFore.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    userId = msg.from.id;
+
+    botFore.sendMessage(chatId, `${msg.chat.username} ջան կարողես ուղարկել մտքերտ...`);
+});
+
+botFore.on('message', (msg) => {
+    const chatId = -1002347536317;
+    var text = '';
+
+    if(msg){
+        text = msg.text;
+        
+        if(text != ''){
+            botFore.sendMessage(chatId, text);
+        }
+    }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
